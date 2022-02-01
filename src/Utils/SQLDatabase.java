d4be78b47d066755d049ad6742944afb7d646823
Robert Rodgers <rodge54@Roberts-MacBook-Pro.local>
2022-02-01 14:02:39 -0600

    Updated sql connection string to use AZURE database

--- src/Utils/SQLDatabase.java
+++ src/Utils/SQLDatabase.java
@@ -3,22 +3,28 @@ package Utils;
 import java.sql.*;
 
 public class SQLDatabase {
-    private static final String url = "jdbc:mysql://localhost:3306/tutoringtrackerdb";
-    private static final String User_Name = "root";
+    //private static final String url = "jdbc:mysql://localhost:3306/tutoringtrackerdb";
+    //private static final String User_Name = "root";
     //WINDOWS LOCAL DB
-    private static final String password = "1234";
+//    private static final String password = "1234";
     //MAC LOCAL DB
-//    private static final String password = "12345678";
+    //private static final String password = "12345678";
     public static Connection conn;
 
     // Connect to Database
     public static void connect() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url, User_Name, password);
-            System.out.println("Connected to MySQL Database");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Class Not Found " + e.getMessage());
+            //AZURE CONNECTION
+            String password = "NotMegatron1234!";
+            String connectionString = "jdbc:sqlserver://robserver54.database.windows.net:1433;database=megatron;user=azureuser@robserver54;password="+password+";encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            conn = DriverManager.getConnection(connectionString);
+            System.out.println("Connected to SQL Server Database");
+            //LOCAL CONNECTION
+            //Class.forName("com.mysql.cj.jdbc.Driver");
+            //conn = DriverManager.getConnection(url, User_Name, password);
+            //System.out.println("Connected to MySQL Database");
+            //} catch (ClassNotFoundException e) {
+            // System.out.println("Class Not Found " + e.getMessage());
         } catch (SQLException e) {
             System.out.println("SQLException: " + e.getMessage());
             System.out.println("SQLState: " + e.getSQLState());
@@ -30,7 +36,7 @@ public class SQLDatabase {
     public static void disconnect() {
         try {
             conn.close();
-            System.out.println("Disconnected From MySQL Database");
+            System.out.println("Disconnected From SQL Server Database");
         } catch (SQLException e) {
             System.out.println("SQLException: " + e.getMessage());
         }
@@ -41,3 +47,4 @@ public class SQLDatabase {
         return conn;
     }
 }
+
